21e20fb65a802fad22240ed1bdd1c527e2353b01
Ardizzone-Mauro <133599406+Ardizzone-Mauro@users.noreply.github.com>
2026-03-28 18:20:10 -0300

    commit inicial

---
+++ SIMA_SOFTWARE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIMA_SOFTWARE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
