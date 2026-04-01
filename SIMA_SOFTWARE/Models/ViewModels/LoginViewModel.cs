f2fcfaef2575f245fe35e16bf20f1dd792c55401
Ardizzone-Mauro <133599406+Ardizzone-Mauro@users.noreply.github.com>
2026-04-01 04:25:11 -0300

    feat: creación de vistas y viewmodels para autenticación

---
+++ SIMA_SOFTWARE/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace SIMA_SOFTWARE.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [EmailAddress(ErrorMessage = "Ingresa un email válido.")]
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "La clave es obligatoria")]
+        public string Clave { get; set; }
+
+        public bool Recordarme { get; set; }
+    }
+}
---
+++ SIMA_SOFTWARE/Models/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SIMA_SOFTWARE.Models.ViewModels
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El campo Apellido es obligatorio.")]
+        [StringLength(50)]
+        public string Apellido { get; set; }
+
+        [EmailAddress(ErrorMessage = "Ingresa un email válido.")]
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "El campo Clave es obligatorio.")]
+        public string Clave { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "El campo Confirmar Clave es obligatorio.")]
+        [Compare("Clave", ErrorMessage = "Las claves no coinciden.")]
+        public string ConfirmarClave { get; set; }
+    }
+}
