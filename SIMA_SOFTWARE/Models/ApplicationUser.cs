25e28106113e7a663591e7d5e16ed235976c6037
Ardizzone-Mauro <133599406+Ardizzone-Mauro@users.noreply.github.com>
2026-04-01 04:24:09 -0300

    feat: extensión de ApplicationUser y migraciones de base de datos

--- SIMA_SOFTWARE/Models/ApplicationUser.cs
+++ SIMA_SOFTWARE/Models/ApplicationUser.cs
@@ -6,6 +6,14 @@ namespace SIMA_SOFTWARE.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(50)]
+        public string? Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? Apellido { get; set; }
+        public string? ImagenUrlPerfil { get; set; }
         //Vinculamos el usuario de acceso con la entidad Cliente del mapeo
         public int? IdCliente { get; set; }
         public virtual Cliente? ClienteUser { get; set; }
