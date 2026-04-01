08c8cbd3ee0143eb42e99fbc39335be0b75ebf69
Ardizzone-Mauro <133599406+Ardizzone-Mauro@users.noreply.github.com>
2026-04-01 04:31:33 -0300

    feat: agregar UsuarioController para autenticación

---
+++ SIMA_SOFTWARE/Controllers/UsuarioController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SIMA_SOFTWARE.Models;
+using SIMA_SOFTWARE.Models.ViewModels;
+
+namespace SIMA_SOFTWARE.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UsuarioController(UserManager<ApplicationUser> userManager,
+                                 SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel Usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                var resultado = await _signInManager.PasswordSignInAsync(Usuario.Email, Usuario.Clave, Usuario.Recordarme, lockoutOnFailure: false);
+                if (resultado.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Inicio de sesión fallido");
+                }
+            }
+            return View(Usuario);
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public  async Task<IActionResult> Registro(RegistroViewModel usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                //Logica para registrar al usuario
+                var nuevoUsuario = new ApplicationUser
+                {
+                    UserName = usuario.Email,
+                    Email = usuario.Email,
+                    Nombre = usuario.Nombre,
+                    Apellido = usuario.Apellido,
+                    ImagenUrlPerfil = "default-profile.png" // Asignar una imagen de perfil predeterminada
+                };
+
+                var resultado = await _userManager.CreateAsync(nuevoUsuario, usuario.Clave);
+
+                if (resultado.Succeeded)
+                {
+                    await _signInManager.SignInAsync(nuevoUsuario, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in resultado.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(usuario);
+        }
+        
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
