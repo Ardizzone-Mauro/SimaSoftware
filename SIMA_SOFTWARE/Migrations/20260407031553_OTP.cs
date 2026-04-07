30a81a3f9e8b6b7b1882f49a6079355b251ff5cb
Ardizzone-Mauro <133599406+Ardizzone-Mauro@users.noreply.github.com>
2026-04-07 03:23:13 -0300

    feat(auth): agregar entidad de código OTP y migración de base de datos
    - Se crea la entidad CodigoRecuperacion
    - Se agregan campos: código, expiración y estado de uso
    - Se genera migración de EF Core para el flujo de recuperación

---
+++ SIMA_SOFTWARE/Migrations/20260407031553_OTP.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIMA_SOFTWARE.Migrations
+{
+    /// <inheritdoc />
+    public partial class OTP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CodigosRecuperacion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Codigo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Expiracion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Usado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CodigosRecuperacion", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CodigosRecuperacion");
+        }
+    }
+}
---
+++ SIMA_SOFTWARE/Models/CodigoRecuperacion.cs
@@ -0,0 +1,12 @@
+﻿namespace SIMA_SOFTWARE.Models
+{
+    public class CodigoRecuperacion
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Codigo { get; set; }
+        public DateTime Expiracion { get; set; }
+        public bool Usado { get; set; }
+
+    }
+}
