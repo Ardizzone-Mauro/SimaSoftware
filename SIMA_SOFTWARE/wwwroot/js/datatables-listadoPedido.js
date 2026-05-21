6fbcd8355190fba0d781b5bfd7f0d24175ded762
Ardizzone-Mauro <133599406+Ardizzone-Mauro@users.noreply.github.com>
2026-05-21 01:27:15 -0300

    feat(HU01): implementa listado operativo de pedidos con DataTables y exportación Excel

---
+++ SIMA_SOFTWARE/wwwroot/js/datatables-listadoPedido.js
@@ -0,0 +1,30 @@
+﻿
+
+    $(document).ready(function () {
+
+        $('#pedidosTable').DataTable({
+
+            language: {
+                url: 'https://cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json'
+            },
+
+            responsive: true,
+
+            pageLength: 5,
+
+            lengthMenu: [
+                [5, 10, 25, 50],
+                [5, 10, 25, 50]
+            ],
+
+            order: [[2, 'desc']], // columna FECHA
+
+            columnDefs: [
+                {
+                    targets: [],
+                    orderable: true
+                }
+            ]
+        });
+
+    });
